a881f2a6b5a1223164f6aacda484b4142397b2a2
hyunjay-dev <nhj3900@gmail.com>
2020-12-09 16:55:14 +0900

    diary ver01

---
+++ back/magic/src/main/java/com/magic/diary/domain/Diary.java
@@ -0,0 +1,98 @@
+package com.magic.diary.domain;
+
+public class Diary {
+	private int diaryID;
+	private String diaryDate;
+	private String diaryQuestions;
+	private String diaryAnswers;
+	private String diaryContents;
+	private String diaryLabels;
+	private String diaryImage;
+	private String diaryEmotions;
+	private String memId;
+	
+	
+	
+	public Diary() {
+	}
+	
+		
+	public Diary(int diaryID, String diaryDate, String diaryQuestions, String diaryLabels, String diaryImage,
+			String diaryEmotions, String memId) {
+		this.diaryID = diaryID;
+		this.diaryDate = diaryDate;
+		this.diaryQuestions = diaryQuestions;
+		this.diaryLabels = diaryLabels;
+		this.diaryImage = diaryImage;
+		this.diaryEmotions = diaryEmotions;
+		this.memId = memId;
+	}
+
+
+
+	public int getDiaryID() {
+		return diaryID;
+	}
+	public void setDiaryID(int diaryID) {
+		this.diaryID = diaryID;
+	}
+	public String getDiaryDate() {
+		return diaryDate;
+	}
+	public void setDiaryDate(String diaryDate) {
+		this.diaryDate = diaryDate;
+	}
+	public String getDiaryQuestions() {
+		return diaryQuestions;
+	}
+	public void setDiaryQuestions(String diaryQuestions) {
+		this.diaryQuestions = diaryQuestions;
+	}
+	public String getDiaryAnswers() {
+		return diaryAnswers;
+	}
+	public void setDiaryAnswers(String diaryAnswers) {
+		this.diaryAnswers = diaryAnswers;
+	}
+	public String getDiaryContents() {
+		return diaryContents;
+	}
+	public void setDiaryContents(String diaryContents) {
+		this.diaryContents = diaryContents;
+	}
+	public String getDiaryLabels() {
+		return diaryLabels;
+	}
+	public void setDiaryLabels(String diaryLabels) {
+		this.diaryLabels = diaryLabels;
+	}
+	public String getDiaryImage() {
+		return diaryImage;
+	}
+	public void setDiaryImage(String diaryImage) {
+		this.diaryImage = diaryImage;
+	}
+	public String getDiaryEmotions() {
+		return diaryEmotions;
+	}
+	public void setDiaryEmotions(String diaryEmotions) {
+		this.diaryEmotions = diaryEmotions;
+	}
+	public String getMemId() {
+		return memId;
+	}
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Diary [diaryID=" + diaryID + ", diaryDate=" + diaryDate + ", diaryQuestions=" + diaryQuestions
+				+ ", diaryAnswers=" + diaryAnswers + ", diaryContents=" + diaryContents + ", diaryLabels=" + diaryLabels
+				+ ", diaryImage=" + diaryImage + ", diaryEmotions=" + diaryEmotions + ", memId=" + memId + "]";
+	}
+	
+	
+	
+}
---
+++ back/magic/src/main/java/com/magic/diary/repo/DiaryDAO.java
@@ -0,0 +1,24 @@
+package com.magic.diary.repo;
+
+import java.util.List;
+
+import com.magic.diary.domain.Diary;
+
+public interface DiaryDAO {
+
+	 public void addDiary(Diary diary) throws Exception;
+	 
+	 public boolean updateDiary(Diary diary) throws Exception;
+	 
+	 public boolean deleteDiary(int diaryId) throws Exception;
+	 
+	 public Diary getDiary(String memId) throws Exception;
+	 
+	 public List<Diary> getAllDiary(String memId) throws Exception;
+	 
+	 public int countDiary(String memId) throws Exception;
+	 
+	 public List<Diary> countLabels(String memId) throws Exception;
+	 
+	 
+}
