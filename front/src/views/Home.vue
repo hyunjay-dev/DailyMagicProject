273a1d1ae1b7b2c38ded4dd7f0f278002c6ac547
hyunjay-dev <nhj3900@gmail.com>
2020-12-10 22:19:40 +0900

    ccc

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <div class="home">
-=======
   <div>
->>>>>>> c1a5af84323e41cbcfa4c03d45a7ef721945718b
     {{info}}
   </div>
 </template>
@@ -14,25 +10,12 @@ export default {
   name: 'Home',
   data () {
     return {
-<<<<<<< HEAD
-      info: null,
-      diaryNo: 2
-=======
       diaryNum: '2',
       info: null
->>>>>>> c1a5af84323e41cbcfa4c03d45a7ef721945718b
     }
   },
   mounted () {
     axios
-<<<<<<< HEAD
-      .get('http://localhost:7000/magic/getDiary/' + this.diaryNo)
-      .then(Response => {
-        this.info = response.data
-      })
-      .catch(e => {
-        conlsole.log(e)
-=======
       .get('http://localhost:7000/magic/getDiary/' + this.diaryNum)
       .then(response => {
         console.log('a')
@@ -40,7 +23,6 @@ export default {
       })
       .catch(e => {
         console.log(e)
->>>>>>> c1a5af84323e41cbcfa4c03d45a7ef721945718b
         this.errored = true
       })
       .finally(() => this.loading = false)
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
     lintOnSave: false
 }
-=======
-  lintOnSave: false
-}
->>>>>>> c1a5af84323e41cbcfa4c03d45a7ef721945718b
