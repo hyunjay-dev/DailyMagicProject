c7fdbbb165e35a3c4017fe5c746272b78100f835
hyunjay-dev <nhj3900@gmail.com>
2020-12-10 22:08:35 +0900

    bbb

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{info}}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      info: null,
+      diaryNo: 2
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:7000/magic/getDiary/' + this.diaryNo)
+      .then(Response => {
+        this.info = response.data
+      })
+      .catch(e => {
+        conlsole.log(e)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
   }
 }
 </script>
